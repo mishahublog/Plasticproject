dbc95f50dbe4c07a4c9066f0380ce696d56d1820
mishahublog <mishahublog@gmail.com>
2023-02-13 23:25:35 +0530

    Registartion completed

--- Litterlog/app_map_cam.R
+++ Litterlog/app_map_cam.R
@@ -3,19 +3,27 @@ library(shiny)
 library(leaflet)
 library(shinymanager)
 library(shinyWidgets)
+library(googlesheets4)
+library(tidyverse)
 
+
+#Setting up the databases
+# userdata
+userdata<- gs4_create(name = "vol_data") #create a data
+#App starts
 ui <- dashboardPage(
-  dashboardHeader(title = "Litter-Log"),
+  dashboardHeader(title = "Litter-Log"), # Title
   dashboardSidebar(),
   dashboardBody(
 
     # Boxes need to be put in a row (or column)
     fillPage(
-      leafletOutput("mymap",height = 700)
+      leafletOutput("mymap",height = 700) # Adding Map
       ),
-    
+    #fab buttons
     fab_button(
       actionButton("show", "Show modal dialog"),
+      actionButton("camera","camera",icon = icon("camera")),
       actionButton(
         inputId = "login",
         label = "Login",
@@ -39,12 +47,13 @@ ui <- dashboardPage(
     )
   )
 )
-
+#server 
 server <- function(input, output) {
  
-  output$mymap <- renderLeaflet({
+  output$mymap <- renderLeaflet({ # map positions 
     leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12) %>% addTiles()
      })
+  #modal 
   observeEvent(input$show,{
 
     showModal(modalDialog(
@@ -92,7 +101,33 @@ server <- function(input, output) {
     
     
       })
+  
+  
+  observeEvent(input$send, {
+    require(tidyverse)
+    #gather info of all the relevant inputs
+    client_df <- c('name', 'dob', 'age', 'phone', 'email') %>% 
+      map_dfc(~ tibble("{.x}" := input[[.x]]))
+    # print(client_df) #this will print a df with one row in the console
+   userbase<-   gs4_get(userdata)
+    sheet_append(userbase, data = client_df)
+  })
+  
+  observeEvent(input$camera,{
+    
+    showModal(modalDialog(
+      
+      
+      
+    ))
+    
+  })
+  
+  
+  
 }
+
+
 
 shinyApp(ui, server)
 
